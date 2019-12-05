6a3b43f18ed8837f40f6ed67b394499993c2712f
Paul <gvr37.p.l.boon@gmail.com>
2019-12-05 15:20:38 +0100

    p

--- main.ts
+++ main.ts
@@ -155,7 +155,8 @@ function updatememarea(){
 fetch('./test.as')
 .then(res => res.text())
 .then(text => {
-    assemblyArea.value = text
+    assemblyArea.value = `add; *i,1; 
+    @i 0`
     execcompile()
     updateinput()
 });
